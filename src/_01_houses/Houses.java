7a8c400d92f1543930998872d9c4a5d5201b6863
chloemcbook <chloemcbook@chloes-mbp.lan>
2020-09-09 18:24:00 -0700

    yes

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -20,18 +20,18 @@ rob.setSpeed(100);
 			Random h = new Random();
 			int i=h.nextInt(3);
 			if (i==0) {
-		house("small",Color.BLUE);
+		drawPointyRoof("small",Color.BLUE);
 		} else if(i==1) {
-			house("medium",Color.CYAN);
+			drawPointyRoof("medium",Color.CYAN);
 		} else {
-	house("large",Color.gray);	
+	drawFlatRoof("large",Color.gray);	
 }
 			
 		}
 
 	}
 
-	void house(String height, Color C) {
+	void drawFlatRoof(String height, Color C) {
 		int h = 100;
 		if (height.equals("small")) {
 			h = 60;
@@ -52,5 +52,28 @@ rob.setSpeed(100);
 		rob.setPenColor(0, 0, 0);
 		rob.turn(270);
 	}
-
+void drawPointyRoof(String height, Color C) {
+	int h = 100;
+	if (height.equals("small")) {
+		h = 60;
+	} else if (height.equals("medium")) {
+		h = 120;
+	} else if (height.equals("large")) {
+		h = 250;
+	}
+	rob.setPenColor(C);
+	rob.move(h);
+	rob.turn(45);
+	rob.move(45);
+	rob.turn(90);
+	rob.move(45);
+	rob.turn(45);
+	rob.move(h);
+	rob.turn(270);
+	rob.setPenColor(0, 250, 0);
+	rob.move(40);
+	rob.setPenColor(0, 0, 0);
+	rob.turn(270);
+}
+	
 }
--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -26,27 +26,33 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-
+String yesh= "https://i.quotev.com/img/q/u/19/9/1/z74odb72gy.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component noice;
 		// 3. use the "createImage()" method below to initialize your Component
-
+noice=createImage(yesh);
 		// 4. add the image to the quiz window
 
-		// 5. call the pack() method on the quiz window
+quizWindow.add(noice);
 
+// 5. call the pack() method on the quiz window
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String answer=JOptionPane.showInputDialog("yesh or nah?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if (answer.equalsIgnoreCase("yesh")) {
+	JOptionPane.showMessageDialog(null, "YESH! YOU ANSWERED CORRECTLY! GOOD! NICE! CONGRATS!");
+}
 		// 8. print "INCORRECT" if the answer is wrong
-
+else {
+	JOptionPane.showMessageDialog(null, "No. You answered incorrectly. Bad.");
+}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizWindow.remove(noice);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+String beautiful="https://assets.puzzlefactory.pl/puzzle/219/653/original.jpg";
 		// 11. add the second image to the quiz window
 
 		// 12. pack the quiz window
